3681bc50aedddd17b9ba55c6bff69591b39e27cb
Jacob Philpott <jacob.philpott@gmx.com>
2023-09-03 12:37:52 +0300

    setting up voice, thanks Shantanu Jana: https://dev.to/shantanu_jana/text-to-speech-converter-with-javascript-30oo

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -1,1 +1,18 @@
-console.log("Keyboard Lesson")
+$(function() {
+
+  const utterance = new SpeechSynthesisUtterance()
+  utterance.voice = window.speechSynthesis.getVoices()[0]
+  welcome = "Hello Micky and welcome to your Keyboard Lesson!"
+
+  $("#intro #start").click(function() {
+
+    utterance.text = welcome
+
+    $("#intro").css("display", "none")
+    $("#keyboard").css("display", "grid")
+
+    window.speechSynthesis.speak(utterance)
+
+  })
+
+})
